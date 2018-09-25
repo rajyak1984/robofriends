467d9e8b0adcfe6d3c2d3d28131bb3d06cb1edd9
rajyak1984 <rashmikolluru@gmail.com>
2018-09-25 13:37:49 +0100

    robofriends project

---
+++ robofriends/robofriends/src/CardList.js
@@ -0,0 +1,25 @@
+import React from'react';
+import Card from './Card.js';
+const CardList = ({robots}) => {
+    
+return (
+    <div> 
+   {
+        robots.map((user, i) => {
+return (
+<Card
+Key={i} 
+id={robots[i].id} 
+name = {robots[i].name} 
+email = {robots[i].email}
+/>
+);
+})
+}
+    
+        
+        
+ </div>
+);
+}
+export default CardList;
